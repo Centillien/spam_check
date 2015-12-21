d320a621a835bff91b7c662f58b4b73d2caf7d96
Gerard Kanters <gkanters@gmail.com>
2015-12-21 11:47:31 +0100

    Geolocation
    The function getRealIpAddr() is used for geolocation on registration and to do some additional checking for spam.

--- start.php
+++ start.php
@@ -50,3 +50,21 @@ function url_get_api_contents($szURL)
 
         return false;
 }
+
+//Get a users real IP address.
+function getRealIpAddr()
+{
+    if (!empty($_SERVER['HTTP_CLIENT_IP']))   //check ip from share internet
+    {
+      $ip=$_SERVER['HTTP_CLIENT_IP'];
+    }
+    elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR']))   //to check ip is pass from proxy
+    {
+      $ip=$_SERVER['HTTP_X_FORWARDED_FOR'];
+    }
+    else
+    {
+      $ip=$_SERVER['REMOTE_ADDR'];
+    }
+    return $ip;
+}
